b2018a294f9907006e16ff5dbd11683dbdc05e39
albertomonge <alberto.monge@polito.it>
2020-08-25 17:17:06 +0200

    Progetto base

---
+++ src/main/java/it/polito/tdp/PremierLeague/FXMLController.java
@@ -0,0 +1,78 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
+package it.polito.tdp.PremierLeague;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.PremierLeague.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnCreaGrafo"
+    private Button btnCreaGrafo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnClassifica"
+    private Button btnClassifica; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnSimula"
+    private Button btnSimula; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmbSquadra"
+    private ComboBox<?> cmbSquadra; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtN"
+    private TextField txtN; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtX"
+    private TextField txtX; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void doClassifica(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCreaGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSimula(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClassifica != null : "fx:id=\"btnClassifica\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSimula != null : "fx:id=\"btnSimula\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert cmbSquadra != null : "fx:id=\"cmbSquadra\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtN != null : "fx:id=\"txtN\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtX != null : "fx:id=\"txtX\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    }
+}
---
+++ src/main/java/it/polito/tdp/PremierLeague/model/Model.java
@@ -0,0 +1,6 @@
+package it.polito.tdp.PremierLeague.model;
+
+
+public class Model {
+	
+}
